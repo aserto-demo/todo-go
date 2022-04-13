eee12bd53fa05ab45c22ef000169fea0a2708dec
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2022-04-13 15:58:12 -0700

    env var names

--- main.go
+++ main.go
@@ -47,13 +47,13 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	authorizerAddr := os.Getenv("AUTHORIZER_ADDRESS")
+	authorizerAddr := os.Getenv("ASERTO_AUTHORIZER_ADDRESS")
 
 	if authorizerAddr == "" {
 		authorizerAddr = "authorizer.prod.aserto.com:8443"
 	}
-	apiKey := os.Getenv("AUTHORIZER_API_KEY")
-	tenantID := os.Getenv("TENANT_ID")
+	apiKey := os.Getenv("ASERTO_AUTHORIZER_API_KEY")
+	tenantID := os.Getenv("ASERTO_TENANT_ID")
 	jwksKeysUrl := os.Getenv("JWKS_URI")
 
 	// Initialize the Aserto Client
